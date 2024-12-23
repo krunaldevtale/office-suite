4a7ffb15f800d760f71d5e4da234986d50122a61
Arshiya143 <arshiya.patel1999@gmail.com>
2024-12-23 18:59:45 +0530

    Added home page

---
+++ public/js/sidebar.js
@@ -0,0 +1,14 @@
+$('.menu-toggle').click(function () {
+    $('#sidebar').toggleClass('-translate-x-full');
+});
+// Get the current path
+const currentPath = window.location.pathname;
+
+// Iterate over all <a> elements in the sidebar
+$("#sidebar nav a").each(function () {
+    // Check if the href matches the current path
+    if ($(this).attr("href") === currentPath) {
+    // Add the 'icon-background' class to the <i> icon inside the <a>
+    $(this).find("i").addClass("icon-background");
+    }
+});

0b29f677732cc18aab5790e65b731a2e6541f1ab
Arshiya143 <arshiya.patel1999@gmail.com>
2024-12-24 15:28:40 +0530

    Added share and profile dropdown

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,22 +1,24 @@
-// Get all "drop" buttons
-const $buttons = $('.drop-button');
+$(document).ready(function () {
+    // Get all "drop" buttons
+    const $buttons = $('.drop-button');
 
-$buttons.each(function() {
-    const $button = $(this);
-    
-    $button.on('click', function(event) {
-    const $dropdown = $button.next('.dropdown');
-    
-    $('.dropdown').not($dropdown).addClass('hidden');
+    $buttons.each(function() {
+        const $button = $(this);
+        
+        $button.on('click', function(event) {
+        const $dropdown = $button.next('.dropdown');
+        
+        $('.dropdown').not($dropdown).addClass('hidden');
 
-    $dropdown.toggleClass('hidden');
-    event.stopPropagation();
+        $dropdown.toggleClass('hidden');
+        event.stopPropagation();
+        });
     });
-});
 
-// Close dropdowns if clicked outside
-$(document).on('click', function(event) {
-    if (!$(event.target).closest('.drop-button').length) {
-    $('.dropdown').addClass('hidden');
-    }
+    // Close dropdowns if clicked outside
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('.drop-button').length) {
+        $('.dropdown').addClass('hidden');
+        }
+    });
 });

6d6172a2c2bb7430315012283f4e3fd62b6a980e
Arshiya143 <arshiya.patel1999@gmail.com>
2024-12-27 12:20:31 +0530

    Added sidebar menu and submenu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     './index.html', // Include your HTML file
+    './src/*.html',
     './dist/**/*.html', // Add your distribution files if applicable
     './styles/**/*.css', // Include your custom CSS
     './src/**/*.{js,ts,jsx,tsx}', // Include your source files if applicable
@@ -13,10 +14,11 @@ module.exports = {
         'royal-blue': '#0272D9',
         'white-smoke': '#FBFBFB',
         'medium-gray': '#FBFBFB4D',
-        'dark-gray': '#333333',
         'dark-charcoal': '#333333',
+        'charcoal-black': '#1D1D1F',
         'dark-gray': '#A9A9A9',
         'light-sky-blue': '#BBE2FA',
+        'transparent-light-sky-blue': '#87CEFA8C',
         'red-orange': '#EE361C',
         'olive': '#DBD77C',
         'light-gray': '#EFEFEF',

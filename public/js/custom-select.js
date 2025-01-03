7010a2e90849490965d1e70b71d3963b22a14e15
Arshiya143 <arshiya.patel1999@gmail.com>
2025-01-03 19:14:42 +0530

    Added document detail, share, version history and export panel

--- public/js/custom-select.js
+++ public/js/custom-select.js
@@ -1,27 +1,33 @@
 $(document).ready(function () {
-    // Set default selected item to "Only you can access"
-    $("#selected-item span:first").text("Only you can access");
-    
-    // Handle the dropdown toggle
-    $("#selected-item").on("click", function (event) {
-        event.stopPropagation();
-        $("#select-options").toggleClass("hidden");
-        $(this).toggleClass("select-arrow-active");
-    });
+    // Handle each custom select individually
+    $(".custom-select").each(function () {
+        const customSelect = $(this);
 
-    // Handle option click
-    $(".option").on("click", function (event) {
-        const selectedText = $(this).text();
-        $("#selected-item span:first").text(selectedText);
-        $("#select-options").addClass("hidden");
-        event.stopPropagation();
+        // Set the default selected item to the first option dynamically
+        const firstOption = customSelect.find(".select-options .option:first").text();
+        customSelect.find(".selected-item span:first").text(firstOption);
+
+        // Handle the dropdown toggle
+        customSelect.find(".selected-item").on("click", function (event) {
+            event.stopPropagation();
+            customSelect.find(".select-options").toggleClass("hidden");
+            $(this).toggleClass("select-arrow-active");
+        });
+
+        // Handle option click
+        customSelect.find(".option").on("click", function (event) {
+            const selectedText = $(this).text();
+            customSelect.find(".selected-item span:first").text(selectedText);
+            customSelect.find(".select-options").addClass("hidden");
+            event.stopPropagation();
+        });
     });
 
     // Close dropdown when clicking outside
     $(document).on("click", function (e) {
         if (!$(e.target).closest(".custom-select").length) {
-        $("#select-options").addClass("hidden");
-        $("#selected-item").removeClass("select-arrow-active");
+            $(".select-options").addClass("hidden");
+            $(".selected-item").removeClass("select-arrow-active");
         }
     });
 });
--- public/js/ribbon.js
+++ public/js/ribbon.js
@@ -1,39 +1,48 @@
 $(document).ready(function () {
+    // Show/hide the ribbon based on mouse position and pin button state
     $('#main-content').on('mousemove', function (e) {
-        var topEdge = $(this).offset().top;
-        var hoverThreshold = 10; 
-        var ribbonHeight = $('.ribbon').outerHeight();
+        var topEdge = $(this).offset().top; 
+        var hoverThreshold = 10;
+        var ribbonHeight = $('.ribbon').outerHeight(); 
 
+        // Check if the pin button is showing the 'eye-off' icon
         if ($('#pin-button i').hasClass('ri-eye-off-line')) {
+            // If mouse is near the top edge, show the ribbon and adjust panel heights
             if (e.pageY - topEdge <= hoverThreshold) {
-                $('.ribbon').show();
-                $('.file-section').css('max-height', 'calc(100vh - 20.3rem)');
+                $('.ribbon').show(); 
+                $('.file-section').css('max-height', 'calc(100vh - 20.3rem)'); 
                 $('.right-panel-content').css('max-height', 'calc(100vh - 25rem)');
-            } else if (e.pageY > (topEdge + ribbonHeight)) {
-                $('.ribbon').hide();
-                $('.file-section').css('max-height', 'calc(100vh - 11.5rem)');
-                $('.right-panel-content').css('max-height', 'calc(100vh - 16rem)');
+            } 
+            // If mouse moves below the ribbon height, hide the ribbon and adjust panel heights
+            else if (e.pageY > (topEdge + ribbonHeight)) {
+                $('.ribbon').hide(); 
+                $('.file-section').css('max-height', 'calc(100vh - 11.5rem)'); 
+                $('.right-panel-content').css('max-height', 'calc(100vh - 16rem)'); 
             }
         }
     });
 
+    // Handle the pin button click event
     $('#pin-button').click(function () {
-        var ribbon = $('.ribbon');
-        var icon = $('#pin-button i');
-        var tooltip = $('#pin-button .tooltip');
+        var ribbon = $('.ribbon'); 
+        var icon = $('#pin-button i'); 
+        var tooltip = $('#pin-button .tooltip'); 
 
+        // If the 'eye-line' icon is active, hide the ribbon and set the appropriate heights
         if (icon.hasClass('ri-eye-line')) {
-            ribbon.hide(); 
+            ribbon.hide();
             icon.removeClass('ri-eye-line').addClass('ri-eye-off-line'); 
-            $('.file-section').css('max-height', 'calc(100vh - 11.5rem)');
-            $('.right-panel-content').css('max-height', 'calc(100vh - 16rem)');
-            tooltip.text('Pin');
-        } else if (icon.hasClass('ri-eye-off-line')) {
+            $('.file-section').css('max-height', 'calc(100vh - 11.5rem)'); 
+            $('.right-panel-content').css('max-height', 'calc(100vh - 16rem)'); 
+            tooltip.text('Pin'); 
+        } 
+        // If the 'eye-off-line' icon is active, show the ribbon and reset the heights
+        else if (icon.hasClass('ri-eye-off-line')) {
             ribbon.show(); 
-            icon.removeClass('ri-eye-off-line').addClass('ri-eye-line');
+            icon.removeClass('ri-eye-off-line').addClass('ri-eye-line'); 
             $('.file-section').css('max-height', 'calc(100vh - 20.3rem)');
             $('.right-panel-content').css('max-height', 'calc(100vh - 25rem)');
-            tooltip.text('Unpin');
+            tooltip.text('Unpin'); 
         }
     });
 });
